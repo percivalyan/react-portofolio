0c00bbbb09155ddc3c2ea2a56bcca18b384decb0
percivalyan <yoryan8888@gmail.com>
2024-06-12 15:36:34 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sub-directory/' // Gantilah dengan sub-directory Anda
 })
